1de07df6a6142f3627501ac8f83a79d38a3a118a
hitcherland <hitcherland@gmail.com>
2019-09-15 22:54:47 +0100

    Updates

---
+++ precache-manifest.29628411d381a4d91440c07db0a3b179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88cefcf17371962c102d2c17cb05321",
+    "url": "/hexsweeper/index.html"
+  },
+  {
+    "revision": "bb637b8d84d238db71cc",
+    "url": "/hexsweeper/static/css/main.161f3d34.chunk.css"
+  },
+  {
+    "revision": "0d0ff0b0d1f7653f61cc",
+    "url": "/hexsweeper/static/js/2.08e2d9c8.chunk.js"
+  },
+  {
+    "revision": "bb637b8d84d238db71cc",
+    "url": "/hexsweeper/static/js/main.280356c6.chunk.js"
+  },
+  {
+    "revision": "ece4651b68ec3186ddb4",
+    "url": "/hexsweeper/static/js/runtime~main.026ffe0d.js"
+  }
+]);
